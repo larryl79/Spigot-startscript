f279b505b4c4d868a0c60b6761a31b03a0b3f44d
larryl79 <larryl79@gmail.com>
2018-04-30 01:37:18 +0100

    V2.27 Help screen addded, minor fixes, colors added

--- server.sh
+++ server.sh
@@ -1,34 +1,33 @@
 #!/bin/sh
 ##########################
 #
-# Server startscript
+# Java Server startscript
 #
-# ver 2.2
+# ver 2.27
 #
 #########################
 
 WHITE='\033[0;37m'
 GREEN='\033[0;32m'
 LRED='\033[1;31m'
+YELLOW='\033[1;33m'
 
 PARAM2=$2
+LOGLINES=40
 
 # config file for wait to stop server
 if [ -e "srv_cfg_stopsec.dat" ]; then
     STOPSEC=`cat srv_cfg_stopsec.dat | awk ' {print $1 }' | grep '^[0-9]'`
     if [ -z $STOPSEC ] || [ $STOPSEC -lt 5 ]; then
-	printf "${GREEN}"
-	printf "Warning: Your waiting seconds are too small at least should be 5.\n"
+	printf "${YELLOW}Warning:${GREEN} Your stop waiting seconds are too small at least should be 5.\n"
 	printf "5">srv_cfg_stopsec.dat
 	printf "${LRED}"
-	printf "Stop seconds seted to default 15 sec in \"srv_cfg_stopsec.dat\"\n"
+	printf "Stop seconds set to default 15 sec in \"srv_cfg_stopsec.dat\"\n"
 	STOPSEC=15
 	printf "${WHITE}"
     fi
-###############################
 else
-    printf "${LRED}"
-    printf "Error: \"srv_cfg_stopsec.dat\" missing.\Creating.\n\n"
+    printf "${YELLOW}Warning:${GREEN} \"${LRED}srv_cfg_stopsec.dat${GREEN}\" missing.\Creating.\n\n"
     printf "15">srv_cfg_stopsec.dat
     printf "${WHITE}"
     STOPSEC=15
@@ -40,20 +39,20 @@ if [ -e "srv_cfg_servername.dat" ]; then
     SZERVER=`cat srv_cfg_servername.dat`
 else
     printf "${GREEN}"
-    printf "Error: \"srv_cfg_servername.dat\" missing.\nExiting.\n\n"
-    printf "${WHITE}"
+    printf "${LRED}Error:${GREEN} \"srv_cfg_servername.dat\" missing.\nExiting.\n\n"
+    printf "${WHITE}\n"
     exit 1
 fi
 
 start(){
-# start script
-rm $SZERVER.pid.old 2>/dev/null
-printf "${GREEN}"
-printf "Starting $SZERVER server in background ...\n"
-java -Xms1024m -Xmx1024m -Dfile.encoding=UTF-8 -jar $SZERVER.jar --noconsole >$SZERVER.log & echo $! > $SZERVER.pid
-printf "PID: "
-cat $SZERVER.pid
-printf "${WHITE}"
+    # start script
+    rm $SZERVER.pid.old 2>/dev/null
+    printf "${GREEN}"
+    printf "Starting $SZERVER server in background ...\n"
+    java -Xms1024m -Xmx1024m -Dfile.encoding=UTF-8 -jar $SZERVER.jar --noconsole >$SZERVER.log & echo $! > $SZERVER.pid
+    printf "PID: "
+    cat $SZERVER.pid
+    printf "${WHITE}\n"
 }
 
 debug(){
@@ -67,7 +66,7 @@ sec=0
 if [ -e $SZERVER.pid ]; then
     pid=`cat $SZERVER.pid`
 else
-    printf "Error: Pid file missing can't stop server or not running.\nExitig.\n"
+    printf "${LRED}Error:${GREEN} Pid file missing can't stop server or not running.\nExitig.\n"
     exit 1
 fi
 
@@ -83,7 +82,7 @@ if [ -e /proc/$pid ]; then
 	    sleep 1;
 	    sec=$((sec + 1));
 	    if [ $sec -eq $STOPSEC ]; then
-		printf "Stopping Timeout. Terminate stopping script. Check for dead thread(s)."
+		printf "Timeout. Terminate stopping script. Check for dead thread(s)."
 		printf "$SZERVER server PID: "
 		cat $SZERVER.pid
 		exit 1
@@ -110,13 +109,13 @@ if [ -e "$SZERVER.log" ]; then
 	f)
 	    printf "Mode: follow, exit with ctrl\+c\n"
 	    printf "${WHITE}"
-	    tail -n40 -f $SZERVER.log 
+	    tail -n$LOGLINES -f $SZERVER.log
 	    exit 1
 	;;
 	t)
 	    printf "Mode: tail\n"
 	    printf "${WHITE}"
-	    tail -n40 $SZERVER.log
+	    tail -n$LOGLINES $SZERVER.log
 	    exit 1
 	;;
     *)
@@ -130,13 +129,46 @@ if [ -e "$SZERVER.log" ]; then
 
 else
     printf "${GREEN}"
-    printf "Error: \"$SZERVER.log\" missing.\nExiting.\n\n"
+    printf "${LRED}Error:${GREEN} \"${LRED}$SZERVER.log${GREEN}\" missing.\nExiting.\n\n"
     printf "${WHITE}"
     exit 1
 fi
 }
 
+help(){
+    printf "\n"
+    printf "${GREEN}"
+    printf "Java Server program manager\n"
+    printf "\n"
+    printf "Help screen\n"
+    printf "\n"
+    syntax
+    printf "\n"
+    printf "Params:\n"
+    printf "\n"
+    printf "${LRED}start${GREEN}	Start server in background\n"
+    printf "${LRED}stop${GREEN}	Stop background running server\n"
+    printf "${LRED}restart${GREEN}	Restart bacground running server\n"
+    printf "${LRED}debug${GREEN}	Start server in foreground. May stop with crtl+c\n"
+    printf "${LRED}log${GREEN}	Show full log of server\n"
+    printf "${LRED}log t${GREEN}	Tail of log file ( last $LOGLINES lines )\n"
+    printf "${LRED}log f${GREEN}	Tail of log file and follow changes. Start with last $LOGLINES lines. Stop with ctrl+c key.\n"
+    printf "${LRED}help${GREEN}	This screen\n"
+    printf "\n"
+    printf "\n"
+    printf "${WHITE}Config files:${GREEN}\n"
+    printf "\n"
+    printf "These files editable by you."
+    printf "\n"
+    printf "${LRED}srv_cfg_servername.dat${GREEN}		Your server program (file)name without extension.\n"
+    printf "				(e.g. MyServer) itt will start MyServer.jar and create MyServer.pid and MyServer.log\n"
+    printf "${LRED}srv_cfg_stopsec.dat${GREEN}		Wait seconds for stop server before this script exit with warn you about dead process. Only numbers in config file. (e.g 10) minimum is 5 second\n"
+    printf "${WHITE}\n"
+}
 
+syntax(){
+    printf "${WHITE}Syntax: ${GREEN}$0 ${WHITE}{ ${LRED}start${WHITE} | ${LRED}stop${WHITE} | ${LRED}restart${WHITE} | ${LRED}debug${WHITE} | ${LRED}log ${WHITE}[${LRED}f${WHITE}|${LRED}t${WHITE}] | ${LRED}help${WHITE} }\n"
+}
 
  case "$1" in
     start)
@@ -153,12 +185,16 @@ fi
 	debug
 	;;
     log)
-
 	log $2
 	;;
+    help)
+	help
+	;;
     *)
-    printf "${GREEN}Usage: $0 {start|stop|restart|debug|log [f|t]}\n"
-    printf "${WHITE}"
+    printf "Server program manager\n"
+    printf "\n"
+    syntax
+    printf "\n"
     exit 1
     ;;
     
